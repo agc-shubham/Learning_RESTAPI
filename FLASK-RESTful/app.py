0f1f12f2872b01fdd182009843f532c1a03f12a2
agcShubham <agc19shubham@gmail.com>
2022-02-10 03:11:33 +0530

    Added authetication and parser

--- FLASK-RESTful/app.py
+++ FLASK-RESTful/app.py
@@ -1,26 +1,54 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
-
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = "Hello"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # Creates /auth
 
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+            type = float,
+            required = True,
+            help = "This field can't be left blank"
+        )
 
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item':None}, 404
+        item = next(filter(lambda x: x['name']==name, items),None)
+        return {'item':item}, 200 if item else 404
 
     def post(self, name):
-        data = request.get_json()
-        print("hello")
-        item = {'name':name, 'price':data}
+        if next(filter(lambda x: x['name']==name, items),None):
+            return {"message": f"an item with name {name} already exists."}
+
+        data = Item.parser.parse_args()
+        item = {'name':name, 'price':data["price"]}
         items.append(item)
         return item, 201
+
+    def delete(self,name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {"message":"Item deleted"}
+
+    def put(self,name):
+        
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name']==name, items),None)
+        if item is None:
+            item = {"name":name, "price":data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 
 class ItemList(Resource):
     def get(self):
@@ -30,4 +58,4 @@ api.add_resource(Item, '/item/<string:name>') #http://127.0.0.1/5000/item/chair
 api.add_resource(ItemList,'/items')
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=5000,debug=True)
